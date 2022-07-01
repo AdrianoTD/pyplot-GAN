238eeaa40ac5f7f60c1bc66ec8c576a74ffd8d29
AdrianoTD <adrianot.dotta@gmail.com>
2022-07-01 12:07:39 -0300

    primeiro push

---
+++ LossSight-GAN/plotting.py
@@ -0,0 +1,15 @@
+import csv
+import numpy as np
+from matplotlib import pyplot as plt
+
+with open('result.csv') as csv_file:
+    csv_reader = csv.DictReader(csv_file)
+    
+    
+    row = next(csv_reader)
+    
+    for row in csv_reader:
+        print(row)
+        
+        #teste git
+        #TTATTT
