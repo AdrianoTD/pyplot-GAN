be2d6ac12cf69fe4d83fb272526ff5fb91d0d4ff
AdrianoTD <adrianot.dotta@gmail.com>
2022-07-05 14:04:21 -0300

    Experimentos com plot

--- LossSight-GAN/plotting.py
+++ LossSight-GAN/plotting.py
@@ -1,7 +1,23 @@
 import csv
 import numpy as np
 from matplotlib import pyplot as plt
+import pandas as pd
 
+data = pd.read_csv('result.csv')
+
+df = pd.DataFrame(data)
+
+x = list(df.iloc[:, 0])
+y = list(df.iloc[:, 0])
+
+plt.bar(x, y, color='b')
+plt.title("DDDDD")
+plt.xlabel("AAAAA")
+plt.ylabel("BBBBB")
+
+plt.show()
+
+"""
 with open('result.csv') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     
@@ -14,3 +30,4 @@ with open('result.csv') as csv_file:
         #teste git
         #TTATTT
         #teste 3
+"""
